163909a75f04e261a5e8c72a7772ea0fe325c0fa
fatih bayram <fatihbayramybs@gmail.com>
2025-11-22 09:51:17 +0300

    small fixings.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+import { ROUTES } from "@/utils/routes";
+
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then(console.log);
+  }, []);
   return <div>Home Page!</div>;
 }
